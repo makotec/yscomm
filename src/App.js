f8fd4f33c18b1de795b7604e32372f052ebaa880
Martin Kottmann <75122031+makotec@users.noreply.github.com>
2022-08-08 21:27:18 +1000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends React.Component {
- componentDidMount(){
-     window.location.href = "https://yscomm.win?src=xx.punkscomic2";
-  }
-
-  render() {
-    return (
-      <div>
-       <h2>Contact</h2>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
