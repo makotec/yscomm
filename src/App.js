80475e5bc818b216326785618865c2dbbc5700eb
Martin Kottmann <75122031+makotec@users.noreply.github.com>
2022-08-09 12:55:37 +1000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -14,11 +14,11 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+        
+          Learn React>
         </a>
       </header>
-    </div> */}
+    </div>
   );
 }
 
